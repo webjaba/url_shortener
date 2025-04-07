84cd759c7d1b80773c0f31cd02b84aad7efa9681
Andrey Kudishkin <kudyshkin.a.k@mail.ru>
2025-04-07 12:20:28 +0300

    chore: add new error

--- internal/app_errors/app_errors.go
+++ internal/app_errors/app_errors.go
@@ -3,6 +3,7 @@ package apperrors
 import "errors"
 
 var (
-	ErrURLNotFound      = errors.New("url not found")
-	ErrURLAlreadyExists = errors.New("url already exists")
+	ErrURLNotFound          = errors.New("url not found")
+	ErrURLAlreadyExists     = errors.New("url already exists")
+	ErrAliasAlreadyOccupied = errors.New("alias already occupied")
 )
